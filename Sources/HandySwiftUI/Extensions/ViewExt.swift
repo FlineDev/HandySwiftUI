a547777f827e76bcfd07d442a1245f412fda2018
Cihat Gündüz <commits@cihatguenduez.de>
2020-08-21 14:50:34 +0200

    Add resign first responder for TextField

--- Sources/HandySwiftUI/Extensions/ViewExt.swift
+++ Sources/HandySwiftUI/Extensions/ViewExt.swift
@@ -2,11 +2,26 @@ import Introspect
 import SwiftUI
 
 extension View {
-    /// Makes views of type `TextField` and `TextEditor` become the first responder if the condition is met.
-    public func becomeFirstResponder(condition: Bool) -> some View {
+    /// Makes views of type `TextField` become the first responder if the condition is met.
+    public func becomeFirstResponder(if condition: Bool) -> some View {
         introspectTextField { textField in
             if condition {
                 textField.becomeFirstResponder()
+            }
+        }
+    }
+
+    /// Resigns views of type `TextField` and `TextEditor` and sets the condition back to false.
+    public func resignFirstResponder(when condition: Binding<Bool>) -> some View {
+        introspectTextField { textField in
+            if condition.wrappedValue {
+                condition.wrappedValue = false
+                textField.resignFirstResponder()
+            }
+        }.introspectScrollView { scollView in
+            if condition.wrappedValue {
+                condition.wrappedValue = false
+                scollView.endEditing(true)
             }
         }
     }
