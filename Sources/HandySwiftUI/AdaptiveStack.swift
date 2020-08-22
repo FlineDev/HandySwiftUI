2e99871e9715b795c517cfaf564e9e098973aed9
Cihat Gündüz <commits@cihatguenduez.de>
2020-08-22 15:49:08 +0200

    Add AdaptiveStack for automatic switch between H/V

---
+++ Sources/HandySwiftUI/AdaptiveStack.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+/// Renders as an `HStack` if on non-compact horizontal size classes, falls back to `VStack` on compact size class.
+public struct AdaptiveStack<Content: View>: View {
+    @Environment(\.horizontalSizeClass)
+    var sizeClass
+
+    let horizontalAlignment: HorizontalAlignment
+    let verticalAlignment: VerticalAlignment
+    let spacing: CGFloat?
+    let content: () -> Content
+
+    /// Creates a new `AdaptiveStack` with same options as `HStack` and `VSTack`.
+    public init(
+        horizontalAlignment: HorizontalAlignment = .center,
+        verticalAlignment: VerticalAlignment = .center,
+        spacing: CGFloat? = nil,
+        @ViewBuilder
+        content: @escaping () -> Content
+    ) {
+        self.horizontalAlignment = horizontalAlignment
+        self.verticalAlignment = verticalAlignment
+        self.spacing = spacing
+        self.content = content
+    }
+
+    public var body: some View {
+        Group {
+            if sizeClass == .compact {
+                VStack(alignment: horizontalAlignment, spacing: spacing, content: content)
+            } else {
+                HStack(alignment: verticalAlignment, spacing: spacing, content: content)
+            }
+        }
+    }
+}
