3c39b9876249ef29ca52b701ec1dd14f6f50257b
Cihat Gündüz <commits@cihatguenduez.de>
2020-11-08 16:44:03 +0100

    Add MultiSelector type

---
+++ Sources/HandySwiftUI/MultiSelectionView.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+
+struct MultiSelectionView<Selectable: Identifiable & Hashable>: View {
+  let options: [Selectable]
+  let optionToString: (Selectable) -> String
+
+  @Binding
+  var selected: Set<Selectable>
+
+  var body: some View {
+    List {
+      ForEach(options) { selectable in
+        Button(action: { toggleSelection(selectable: selectable) }) {
+          HStack {
+            Text(optionToString(selectable)).foregroundColor(.label)
+            Spacer()
+            if selected.contains { $0.id == selectable.id } {
+              Image(systemName: "checkmark").foregroundColor(.accentColor)
+            }
+          }
+        }
+        .tag(selectable.id)
+      }
+    }
+    .listStyle(GroupedListStyle())
+  }
+
+  private func toggleSelection(selectable: Selectable) {
+    if let existingIndex = selected.firstIndex(where: { $0.id == selectable.id }) {
+      selected.remove(at: existingIndex)
+    }
+    else {
+      selected.insert(selectable)
+    }
+  }
+}
+
+struct MultiSelectionView_Previews: PreviewProvider {
+  struct IdentifiableString: Identifiable, Hashable {
+    let string: String
+    var id: String { string }
+  }
+
+  @State static var selected: Set<IdentifiableString> = Set(["A", "C"].map { IdentifiableString(string: $0) })
+
+  static var previews: some View {
+    NavigationView {
+      MultiSelectionView(
+        options: ["A", "B", "C", "D"].map { IdentifiableString(string: $0) },
+        optionToString: { $0.string },
+        selected: $selected
+      )
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
+}
---
+++ Sources/HandySwiftUI/MultiSelector.swift
@@ -0,0 +1,77 @@
+import Foundation
+import SwiftUI
+
+/// A selector that supports choosing multiple options across a given set of options. Use `Picker` for choosing exactly one option.
+public struct MultiSelector<LabelView: View, Selectable: Identifiable & Hashable>: View {
+  /// The view to use as the label for the multi-selector.
+  public let label: LabelView
+
+  /// The possible options to choose from.
+  public let options: [Selectable]
+
+  /// A closure that converts the given options to their String representation.
+  public let optionToString: (Selectable) -> String
+
+  /// A set of the currently selected options.
+  public var selected: Binding<Set<Selectable>>
+
+  private var formattedSelectedListString: String {
+    ListFormatter.localizedString(byJoining: selected.wrappedValue.map { optionToString($0) })
+  }
+
+  public var body: some View {
+    NavigationLink(destination: multiSelectionView()) {
+      HStack {
+        label
+        Spacer()
+        Text(formattedSelectedListString)
+          .foregroundColor(.secondaryLabel)
+          .multilineTextAlignment(.trailing)
+      }
+    }
+  }
+
+  public init(
+    label: LabelView,
+    options: [Selectable],
+    selected: Binding<Set<Selectable>>,
+    optionToString: @escaping (Selectable) -> String
+  ) {
+    self.label = label
+    self.options = options
+    self.selected = selected
+    self.optionToString = optionToString
+  }
+
+  private func multiSelectionView() -> some View {
+    MultiSelectionView(
+      options: options,
+      optionToString: optionToString,
+      selected: selected
+    )
+  }
+}
+
+struct MultiSelector_Previews: PreviewProvider {
+  struct IdentifiableString: Identifiable, Hashable {
+    let string: String
+    var id: String { string }
+  }
+
+  @State static var selected: Set<IdentifiableString> = Set(["A", "C"].map { IdentifiableString(string: $0) })
+
+  static var previews: some View {
+    NavigationView {
+      Form {
+        MultiSelector<Text, IdentifiableString>(
+          label: Text("MOCK: Multiselect"),
+          options: ["A", "B", "C", "D"].map { IdentifiableString(string: $0) },
+          selected: $selected,
+          optionToString: { $0.string }
+        )
+      }
+      .navigationTitle("MOCK: Title")
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
+}
