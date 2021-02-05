5bda0528200d1e1020f0bbc10fcf786e7268c5b1
Cihat Gündüz <cg@papershift.com>
2021-02-05 15:58:25 +0100

    Add possibility to preview on tablets instead

--- Sources/HandySwiftUI/Modifiers/PreviewVariant.swift
+++ Sources/HandySwiftUI/Modifiers/PreviewVariant.swift
@@ -7,32 +7,44 @@ fileprivate struct PreviewVariant: ViewModifier {
     case large = "iPhone 12 Pro Max"
   }
 
-  static let spacious: PreviewVariant = PreviewVariant(
-    phoneSize: .large,
-    colorScheme: .dark,
-    sizeCategory: .small
-  )
+  enum PadSize: String {
+    case small = "iPad (8th generation)"
+    case medium = "iPad Air (4th generation)"
+    case large = "iPad Pro (12.9-inch) (4th generation)"
+  }
 
-  static let normal: PreviewVariant = PreviewVariant(
-    phoneSize: .medium,
-    colorScheme: .light,
-    sizeCategory: .medium
-  )
+  static func spacious(tablet: Bool) -> PreviewVariant {
+    PreviewVariant(
+      deviceName: tablet ? PadSize.large.rawValue : PhoneSize.large.rawValue,
+      colorScheme: .dark,
+      sizeCategory: .small
+    )
+  }
 
-  static let narrow: PreviewVariant = PreviewVariant(
-    phoneSize: .small,
-    colorScheme: .dark,
-    sizeCategory: .extraExtraExtraLarge
-  )
+  static func medium(tablet: Bool) -> PreviewVariant {
+    PreviewVariant(
+      deviceName: tablet ? PadSize.medium.rawValue : PhoneSize.medium.rawValue,
+      colorScheme: .light,
+      sizeCategory: .medium
+    )
+  }
 
-  let phoneSize: PhoneSize
+  static func narrow(tablet: Bool) -> PreviewVariant {
+    PreviewVariant(
+      deviceName: tablet ? PadSize.small.rawValue : PhoneSize.small.rawValue,
+      colorScheme: .dark,
+      sizeCategory: .extraExtraExtraLarge
+    )
+  }
+
+  let deviceName: String
   let colorScheme: ColorScheme
   let sizeCategory: ContentSizeCategory
 
   func body(content: Content) -> some View {
     let details = "\(sizeCategory)-\(String(describing: colorScheme).capitalized)"
-    return content.previewDevice(PreviewDevice(rawValue: phoneSize.rawValue))
-      .previewDisplayName("\(phoneSize.rawValue): \(details)")
+    return content.previewDevice(PreviewDevice(rawValue: deviceName))
+      .previewDisplayName("\(deviceName): \(details)")
       .preferredColorScheme(colorScheme)
       .environment(\.sizeCategory, sizeCategory)
   }
@@ -44,14 +56,14 @@ extension View {
   /// - Parameters:
   ///   - narrowLanguage: Override the default preview language setting for the narrow preview with an custom language code.
   ///   - spaciousLanguage: Override the default preview language setting for the spacious preview with an custom language code.
-  public func previewScreens(narrowLanguage: String? = nil, spaciousLanguage: String? = nil) -> some View {
+  public func previewScreens(tablet: Bool = false, narrowLanguage: String? = nil, spaciousLanguage: String? = nil) -> some View {
     Group {
-      modifier(PreviewVariant.narrow)
+      modifier(PreviewVariant.narrow(tablet: tablet))
         .applyIf(narrowLanguage != nil) {
           $0.environment(\.locale, .init(identifier: narrowLanguage!))
         }
-      modifier(PreviewVariant.normal)
-      modifier(PreviewVariant.spacious)
+      modifier(PreviewVariant.medium(tablet: tablet))
+      modifier(PreviewVariant.spacious(tablet: tablet))
         .applyIf(spaciousLanguage != nil) {
           $0.environment(\.locale, .init(identifier: spaciousLanguage!))
         }
@@ -63,7 +75,7 @@ extension View {
   /// - Parameters:
   ///   - narrowLanguage: Override the default preview language setting for the narrow preview with an custom language code.
   ///   - spaciousLanguage: Override the default preview language setting for the spacious preview with an custom language code.
-  public func previewComponents(narrowLanguage: String? = nil, spaciousLanguage: String? = nil) -> some View {
-    previewScreens(narrowLanguage: narrowLanguage, spaciousLanguage: spaciousLanguage).previewLayout(.sizeThatFits)
+  public func previewComponents(tablet: Bool = false, narrowLanguage: String? = nil, spaciousLanguage: String? = nil) -> some View {
+    previewScreens(tablet: tablet, narrowLanguage: narrowLanguage, spaciousLanguage: spaciousLanguage).previewLayout(.sizeThatFits)
   }
 }
