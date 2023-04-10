ad5b81924b439f17dcac84125ed57a071dfad02f
Cihat Gündüz <commits@cihatguenduez.de>
2023-04-10 19:07:07 +0200

    Update Package manifest to Swift 5.7 + point to HandySwift@main

--- Package.swift
+++ Package.swift
@@ -1,23 +1,29 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "HandySwiftUI",
-  platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14)],
-  products: [
-    .library(name: "HandySwiftUI", targets: ["HandySwiftUI"])
-  ],
-  dependencies: [
-    // Handy Swift features that didn't make it into the Swift standard library.
-    .package(name: "HandySwift", url: "https://github.com/FlineDev/HandySwift.git", from: "3.2.1"),
+   name: "HandySwiftUI",
+   platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14)],
+   products: [
+      .library(name: "HandySwiftUI", targets: ["HandySwiftUI"])
+   ],
+   dependencies: [
+      // Handy Swift features that didn't make it into the Swift standard library.
+      .package(url: "https://github.com/FlineDev/HandySwift.git", branch: "main"),
 
-    // Introspect underlying UIKit components from SwiftUI.
-    .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.0"),
-  ],
-  targets: [
-    .target(name: "HandySwiftUI", dependencies: ["HandySwift", "Introspect"]),
-    .testTarget(name: "HandySwiftUITests", dependencies: ["HandySwiftUI"]),
-  ]
+      // Introspect underlying UIKit components from SwiftUI.
+      .package(url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.0"),
+   ],
+   targets: [
+      .target(
+         name: "HandySwiftUI",
+         dependencies: [
+            .product(name: "HandySwift", package: "HandySwift"),
+            .product(name: "Introspect", package: "SwiftUI-Introspect"),
+         ]
+      ),
+      .testTarget(name: "HandySwiftUITests", dependencies: ["HandySwiftUI"]),
+   ]
 )
