a26a0a9e4af0d6d7a1305634b61866e680c0141c
Cihat Gündüz <commits@cihatguenduez.de>
2023-08-10 11:51:48 +0200

    Add option to not mute the label of a LabeledContentView

--- Sources/HandySwiftUI/Styles/VerticalLabeledContentStyle.swift
+++ Sources/HandySwiftUI/Styles/VerticalLabeledContentStyle.swift
@@ -2,17 +2,22 @@ import SwiftUI
 
 public struct VerticalLabeledContentStyle: LabeledContentStyle {
    var spacing: CGFloat
+   var muteLabel: Bool
 
-   public init(spacing: CGFloat) {
+   public init(spacing: CGFloat, muteLabel: Bool) {
       self.spacing = spacing
+      self.muteLabel = muteLabel
    }
 
    public func makeBody(configuration: Configuration) -> some View {
       VStack(alignment: .leading, spacing: self.spacing) {
          configuration.label
-            .font(.footnote)
-            .foregroundColor(.secondary)
-            .minimumScaleFactor(0.75)
+            .applyIf(self.muteLabel) { label in
+               label
+                  .font(.footnote)
+                  .foregroundColor(.secondary)
+                  .minimumScaleFactor(0.75)
+            }
 
          configuration.content
       }
@@ -20,8 +25,8 @@ public struct VerticalLabeledContentStyle: LabeledContentStyle {
 }
 
 extension LabeledContentStyle where Self == VerticalLabeledContentStyle {
-   public static func vertical(spacing: CGFloat = 4) -> VerticalLabeledContentStyle {
-      VerticalLabeledContentStyle(spacing: spacing)
+   public static func vertical(spacing: CGFloat = 4, muteLabel: Bool = true) -> VerticalLabeledContentStyle {
+      VerticalLabeledContentStyle(spacing: spacing, muteLabel: muteLabel)
    }
 }
 
