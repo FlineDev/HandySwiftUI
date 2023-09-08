77587003e0d7facd88fa40d0b99ff2e31778aa94
Cihat Gündüz <commits@cihatguenduez.de>
2023-09-08 16:47:01 +0200

    Support current stable releases

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.8
 import PackageDescription
 
 let package = Package(
    name: "HandySwiftUI",
-   platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
+   platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
    products: [.library(name: "HandySwiftUI", targets: ["HandySwiftUI"])],
    dependencies: [.package(url: "https://github.com/FlineDev/HandySwift.git", branch: "main")],
    targets: [
