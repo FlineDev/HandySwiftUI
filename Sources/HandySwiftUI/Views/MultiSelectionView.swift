43b5fbf4e7d91c0b8241375754bd1aacb9844713
Cihat Gündüz <commits@cihatguenduez.de>
2023-09-25 21:04:49 +0200

    Implement custom multi selection sheet view for macOS

--- Sources/HandySwiftUI/Views/MultiSelectionView.swift
+++ Sources/HandySwiftUI/Views/MultiSelectionView.swift
@@ -14,6 +14,7 @@ struct MultiSelectionView<Selectable: Identifiable & Hashable>: View {
       List {
          Section {
             ForEach(self.options) { selectable in
+               #if !os(macOS)
                Button(action: { toggleSelection(selectable: selectable) }) {
                   HStack {
                      Text(optionToString(selectable)).foregroundColor(.label)
@@ -23,8 +24,13 @@ struct MultiSelectionView<Selectable: Identifiable & Hashable>: View {
                      }
                   }
                }
-               .buttonStyle(.plain)
                .tag(selectable.id)
+               #else
+               Toggle(optionToString(selectable), isOn: self.boolBinding(selectable: selectable))
+                  .toggleStyle(.switch)
+                  .frame(height: 22)
+                  .tag(selectable.id)
+               #endif
             }
          }
 
@@ -37,8 +43,24 @@ struct MultiSelectionView<Selectable: Identifiable & Hashable>: View {
          }
          #endif
       }
+      #if os(macOS)
+      .frame(height: min((NSScreen.main?.frame.height ?? 100_000) - 54, Double(self.options.count) * 22 + 104))
+      #endif
    }
-   
+
+   private func boolBinding(selectable: Selectable) -> Binding<Bool> {
+      Binding(
+         get: { self.selected.contains(selectable) },
+         set: { isOn in
+            if isOn {
+               self.selected.insert(selectable)
+            } else {
+               self.selected.remove(selectable)
+            }
+         }
+      )
+   }
+
    private func toggleSelection(selectable: Selectable) {
       if let existingIndex = selected.firstIndex(where: { $0.id == selectable.id }) {
          selected.remove(at: existingIndex)
