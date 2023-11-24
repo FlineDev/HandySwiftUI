c3a5e0531199580894c0668693c4dc417cea1dcb
Cihat Gündüz <commits@cihatguenduez.de>
2023-11-24 12:43:36 +0100

    Revert "Remove iconAngle param from H/V custom label styles"
    This reverts commit bca0cf8947ce7a83dcb9f2eac54cb77d4f878d89.

--- Sources/HandySwiftUI/Styles/VerticalLabelStyle.swift
+++ Sources/HandySwiftUI/Styles/VerticalLabelStyle.swift
@@ -4,18 +4,21 @@ public struct VerticalLabelStyle: LabelStyle {
    var spacing: CGFloat
    var iconColor: Color?
    var iconFont: Font?
+   var iconAngle: Angle?
 
-   public init(spacing: CGFloat, iconColor: Color?, iconFont: Font?) {
+   public init(spacing: CGFloat, iconColor: Color?, iconFont: Font?, iconAngle: Angle?) {
       self.spacing = spacing
       self.iconColor = iconColor
       self.iconFont = iconFont
+      self.iconAngle = iconAngle
    }
 
    public func makeBody(configuration: Configuration) -> some View {
       VStack(alignment: .center, spacing: self.spacing) {
          configuration.icon
-            .ifLet(self.iconColor) { $0.foregroundStyle($1) }
-            .ifLet(self.iconFont) { $0.font($1) }
+            .applyIf(self.iconColor != nil) { $0.foregroundStyle(self.iconColor!) }
+            .applyIf(self.iconFont != nil) { $0.font(self.iconFont!) }
+            .applyIf(self.iconAngle != nil) { $0.rotationEffect(self.iconAngle!) }
 
          configuration.title
       }
@@ -26,9 +29,10 @@ extension LabelStyle where Self == VerticalLabelStyle {
    public static func vertical(
       spacing: CGFloat = 4,
       iconColor: Color? = .accentColor,
-      iconFont: Font? = nil
+      iconFont: Font? = nil,
+      iconAngle: Angle? = nil
    ) -> VerticalLabelStyle {
-      VerticalLabelStyle(spacing: spacing, iconColor: iconColor, iconFont: iconFont)
+      VerticalLabelStyle(spacing: spacing, iconColor: iconColor, iconFont: iconFont, iconAngle: iconAngle)
    }
 }
 
