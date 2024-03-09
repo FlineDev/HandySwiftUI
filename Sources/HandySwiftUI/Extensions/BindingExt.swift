03491391bddb7a873383e7dd354d216c2277b0ab
Cihat Gündüz <commits@cihatguenduez.de>
2024-03-09 18:52:07 +0100

    Improve String Optional Binding nil-coalesing operator

--- Sources/HandySwiftUI/Extensions/BindingExt.swift
+++ Sources/HandySwiftUI/Extensions/BindingExt.swift
@@ -1,5 +1,17 @@
 import SwiftUI
 
+/// Types that conform to this protocol provide a placeholder value that can be used in place of `nil` for Optionals of this type.
+/// For example, this can be useful to pass a ``Binding`` of an ``Optional`` value to a view that requires a non-optional value like ``TextField``.
+public protocol NilPlaceholdable: Equatable {
+   static var nilPlaceholderValue: Self { get }
+}
+
+// Common Conforming Types
+extension String: NilPlaceholdable {
+   public static var nilPlaceholderValue: String { "" }
+}
+
+// Actual Binding Extensions
 public prefix func ! (value: Binding<Bool>) -> Binding<Bool> {
    Binding<Bool>(
       get: { !value.wrappedValue },
@@ -7,9 +19,9 @@ public prefix func ! (value: Binding<Bool>) -> Binding<Bool> {
    )
 }
 
-public func ?? <T>(lhs: Binding<T?>, rhs: T) -> Binding<T> {
+public func ?? <T: NilPlaceholdable>(lhs: Binding<T?>, rhs: T) -> Binding<T> {
    Binding(
       get: { lhs.wrappedValue ?? rhs },
-      set: { lhs.wrappedValue = $0 }
+      set: { lhs.wrappedValue = $0 == T.nilPlaceholderValue ? nil : $0 }
    )
 }
