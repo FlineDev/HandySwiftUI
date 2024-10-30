615aa53700f52356e3c4e4e20bad94a9f59b7ef0
Cihat Gündüz <commits@cihatguenduez.de>
2024-10-30 08:21:21 +0100

    Bump minimum HandySwift required version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
    defaultLocalization: "en",
    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .visionOS(.v1), .watchOS(.v9)],
    products: [.library(name: "HandySwiftUI", targets: ["HandySwiftUI"])],
-   dependencies: [.package(url: "https://github.com/FlineDev/HandySwift.git", from: "4.2.0")],
+   dependencies: [.package(url: "https://github.com/FlineDev/HandySwift.git", from: "4.3.0")],
    targets: [
       .target(
          name: "HandySwiftUI",
