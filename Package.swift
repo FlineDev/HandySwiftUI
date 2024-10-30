eff878079026599cd8d1765a74b78f3d13279b81
Cihat Gündüz <commits@cihatguenduez.de>
2024-10-30 08:00:32 +0100

    Update .spi.yml to use Swift 6 & other minor cleanup

--- Package.swift
+++ Package.swift
@@ -5,11 +5,7 @@ let package = Package(
    name: "HandySwiftUI",
    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .visionOS(.v1), .watchOS(.v9)],
    products: [.library(name: "HandySwiftUI", targets: ["HandySwiftUI"])],
-   dependencies: [
-//      // TODO: continue by running `swift package --disable-sandbox preview-documentation --target HandySwiftUI` and fixing build issues
-//      .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-      .package(url: "https://github.com/FlineDev/HandySwift.git", from: "4.2.0")
-   ],
+   dependencies: [.package(url: "https://github.com/FlineDev/HandySwift.git", from: "4.2.0")],
    targets: [
       .target(
          name: "HandySwiftUI",
